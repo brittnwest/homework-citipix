6e1300d11a8ba8d92320c3b4789f111eba3568fb
brittanywest <brittany.west@concur.com>
2017-04-09 12:23:41 -0700

    Change to drop down

--- js/index.js
+++ js/index.js
@@ -1,42 +1,52 @@
 
 //on load, prevent default
 $(document).ready(function(){
+//set options
 
+var cities  = ['Select a city...','New York City','San Francisco','Los Angeles','Austin','Sydney']
+for(index=0; index < cities.length; ++index){
+  var option = new Option(cities[index], index)
+  $('select').append($(option));
+  }
 });
-//check for submit
 
-$(".container").on("submit",function(event){
+//check for change
+$('select').on('change', function(event){
+   event.preventDefault();
+   var city
+   var img
+   var src
 
-  event.preventDefault();
+//get selected city
+    city = $('select option:selected').val()
 
-  var city
-  var img
-  var src
-//get input value
-    city =  $("#city-type").val()
+    console.log(city)
 //determine image to display based on input
-    if (city === "New York" || city === "New York City" || city === "NYC"){
+    if (city === '0'){
+      //do nothing
+    } else if (city === '1') {
       img = 'nyc'
 
       setImage(img)
 
-    } else if (city === "San Francisco" || city === "SF" || city === "Bay Area" ) {
+    } else if (city === '2') {
       img = 'sf'
       setImage(img)
     }
-    else if (city === "Los Angeles" || city === "LA" || city === "LAX" ) {
+    else if (city === '3') {
       img = 'la'
       setImage(img)
     }
-    else if (city === "Austin" || city === "ATX") {
+    else if (city === '4') {
       img = 'austin'
       setImage(img)
     }
-    else if (city === "Syndey" || city === "SYD") {
+    else if (city === '5') {
       img = 'sydney'
       setImage(img)
     }
-});
+  });
+
 
 function setImage(img){
   //set background image
@@ -45,6 +55,5 @@ function setImage(img){
 
   $("body").css("background", "url("+src+") no-repeat");
   $("body").css("background-size", "100%");
-  //reset input form
-  $("#city-type").val('');
+
 }
